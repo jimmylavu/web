c4c3ab2e22f4a29092238fdf01eeef5727bbac3f
jimmylavu <jsumalpong43@gmail.com>
2024-05-21 00:04:13 +0800

    Update settings.py

--- cosmo/cosmo/settings.py
+++ cosmo/cosmo/settings.py
@@ -109,12 +109,14 @@ WSGI_APPLICATION = 'cosmo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+if not DEBUG:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+        }}
 
 
 # Password validation
